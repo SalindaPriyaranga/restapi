41b9dc0f508b6918c73c8493f04b6813f976556b
SalindaPriyaranga <34094843+SalindaPriyaranga@users.noreply.github.com>
2021-09-25 10:36:31 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 module.exports = {
-  
-    // HOST: "",
-    // USER: "root",
-    // PASSWORD: "",
-    // DB: "ec_training",
-    // dialect: "mysql",
-    HOST: process.env.DB_HOST,
-    USER: process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    DB: process.env.DB_NAME,
-    dialect: process.env.DB_DIALECT,
     
+    // HOST: process.env.DB_HOST,
+    // USER: process.env.USER,
+    // PASSWORD: process.env.PASSWORD,
+    // DB: process.env.DB_NAME,
+    // dialect: process.env.DB_DIALECT,
+
+    HOST: "",
+    USER: "root",
+    PASSWORD: "",
+    DB: "ec_training",
+    dialect: "mysql",
+
     pool: {
         max: 5,
         min: 0,
